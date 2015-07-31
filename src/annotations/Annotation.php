0c9f8798b2f096e577913ac87e5eb3e78b6d24bc
RaaghuKR <raghavendra@icircle.co.in>
2015-07-31 10:49:37 +0530

    completed Annotation class

--- src/annotations/Annotation.php
+++ src/annotations/Annotation.php
@@ -5,6 +5,11 @@ namespace icircle\annotations;
 class Annotation{
     private static $_annotations = array();
 
+    const APPLIED_ON = "appliedOn";
+    const TYPE       = "type";
+    const ALLOW_NULL = "allowNull";
+    const VALIDATE   = "validate";
+
     /**
      * Method to register Annotation Definitions
      * @param $source
@@ -101,23 +106,26 @@ class Annotation{
 
         if(is_string($memberName) && $memberName != "*"){
             $reflectionProperty = null;
+            $reflectionPropertyType = null;
             try{
                 $reflectionProperty = $reflectionClass->getProperty($memberName);
+                $reflectionPropertyType = "property";
             }catch (\ReflectionException $re){
                 try{
                     $reflectionProperty = $reflectionClass->getMethod($memberName);
+                    $reflectionPropertyType = "method";
                 }catch (\ReflectionException $re){
                     throw new \Exception("Unable to get annotataions , Class doesn't contain specified member [$className:$memberName]");
                 }
             }
 
             $propertyDocComment = $reflectionProperty->getDocComment();
-            return self::getAnnotationsFromDocComment($propertyDocComment);
+            return self::getAnnotationsFromDocComment($propertyDocComment,$reflectionPropertyType);
         }
 
         $annotations = array();
         $classDocComment = $reflectionClass->getDocComment();
-        $classAnnotations = self::getAnnotationsFromDocComment($classDocComment);
+        $classAnnotations = self::getAnnotationsFromDocComment($classDocComment,"class");
 
         $annotations["class"] = $classAnnotations;
 
@@ -127,7 +135,7 @@ class Annotation{
             $reflectionProperties = $reflectionClass->getProperties();
             foreach($reflectionProperties as $reflectionProperty){
                 $propertyDocComment = $reflectionProperty->getDocComment();
-                $propertyAnnotations = self::getAnnotationsFromDocComment($propertyDocComment);
+                $propertyAnnotations = self::getAnnotationsFromDocComment($propertyDocComment,"property");
                 if($propertyAnnotations !== FALSE){
                     $memberAnnotations[$reflectionProperty->getName()] = $propertyAnnotations;
                 }
@@ -136,7 +144,7 @@ class Annotation{
             $reflectionMethods = $reflectionClass->getMethods();
             foreach($reflectionMethods as $reflectionMethod){
                 $methodDocComment = $reflectionMethod->getDocComment();
-                $methodAnnotations = self::getAnnotationsFromDocComment($methodDocComment);
+                $methodAnnotations = self::getAnnotationsFromDocComment($methodDocComment,"method");
                 if($methodAnnotations !== FALSE){
                     $memberAnnotations[$reflectionMethod->getName()] = $methodAnnotations;
                 }
@@ -146,8 +154,169 @@ class Annotation{
         return $annotations;
     }
 
-    static private function getAnnotationsFromDocComment($docComment){
-        return array();
+    /**
+     * @param $docComment
+     * @param $type
+     * @return array
+     *
+     * " * @key"
+     * " * @key value "
+     * " *@key value  "
+     * " * @key value Description"
+     * " * @key (value1,...)"
+     * " * @key(value1,...)"
+     * " * @key "value with spaces" "
+     */
+
+    static private function getAnnotationsFromDocComment($docComment,$type){
+
+        $docCommentLines = preg_split("[\r\n]",$docComment);
+
+        $annotations = array();
+        foreach($docCommentLines as $docCommentLine){
+
+            if(strpos($docCommentLine,"@") === FALSE){
+                continue;
+            }
+
+            $tokens = token_get_all("<?php ".$docCommentLine);
+
+            // $tokens will be of form
+            // array(array(T_OPEN_TAG ,"<?php",0),
+            //       array(T_WHITESPACE," ",0),
+            //       "*",
+            //       array(T_WHITESPACE," ",0),  // this token is optional
+            //       "@",
+            //       array(T_STRING,{annotationName},0),
+            //       array(T_WHITESPACE," ",0),  // this token is optional
+            //       {annotationValue} | array(T_STRING,{annotationValue},0) | array(T_CONSTANT_ENCAPSED_STRING ,{annotationValue},0) | array(T_LNUMBER ,{annotationValue},0) | array(T_DNUMBER ,{annotationValue},0) | "(",
+            //           {annotationValue} | array(T_STRING,{annotationValue},0) | array(T_CONSTANT_ENCAPSED_STRING ,{annotationValue},0) | array(T_LNUMBER ,{annotationValue},0) | array(T_DNUMBER ,{annotationValue},0) | ","  // if previous token is "("
+            //       ")",
+            //       // rest of the tokens can be ignored
+            //   )
+
+            if($tokens[2] !== "*"){
+                continue;
+            }
+
+            $annotationNameTokenIndex = 4;
+            if(is_array($tokens[3]) && $tokens[3][0] == T_WHITESPACE){
+                $annotationNameTokenIndex = 5;
+            }
+
+            // @ is not the prefix of annotationName , OR
+            // if annotationName is not a string , OR
+            // if count of tokens is less , then this line does not contain annotations
+            if($tokens[$annotationNameTokenIndex-1] != "@" ||
+                !(is_array($tokens[$annotationNameTokenIndex] && $tokens[$annotationNameTokenIndex][0] == T_STRING))){
+                continue;
+            }
+
+            $annotationName = $tokens[$annotationNameTokenIndex][1];
+
+            $annotationDefinition = self::$_annotations[$annotationName];
+
+            if(!isset($annotationDefinition)){
+                continue;
+            }
+
+            // check for appliedOn
+            $_appliedOn = $annotationDefinition[self::APPLIED_ON];
+            if(isset($_appliedOn)){
+                if(trim(strtoupper($_appliedOn)) != "ALL" && trim(strtoupper($_appliedOn)) != trim(strtoupper($type))){
+                    continue;
+                }
+            }
+
+            $annotationValue = null;
+            $hasSpaceBetweenNameAndValue = false;
+            $errorInParsing = false;
+
+            for($i = $annotationNameTokenIndex+1 ; $i<count($tokens);$i++){
+                $token = $tokens[$i];
+
+                if(is_string($token)){
+                    if($annotationValue == null){
+                        if($token == "("){
+                            $annotationValue = array();
+                            continue;
+                        }else{
+                            if($hasSpaceBetweenNameAndValue){
+                                $annotationValue = $token;
+                                break;
+                            }else{
+                                //error
+                                $errorInParsing = true; break;
+                            }
+                        }
+                    }else{
+                        if($token == "," && count($annotationValue) > 0){
+                            continue;
+                        }else if($token == ")"){
+                            break;
+                        }else{
+                            //error
+                            $errorInParsing = true; break;
+                        }
+                    }
+                }else{
+                    if($token[0] == T_CONSTANT_ENCAPSED_STRING){
+                        $token[1] = stripcslashes($token[1]);
+                    }
+
+                    if($annotationValue == null){
+                        if($token[0] == T_WHITESPACE){
+                            $hasSpaceBetweenNameAndValue = true;
+                            continue;
+                        }else{
+                            if($hasSpaceBetweenNameAndValue){
+                                $annotationValue = $token[1];
+                                break;
+                            }else{
+                                //error
+                                $errorInParsing = true; break;
+                            }
+                        }
+                    }else{
+                        if($token[0] == T_WHITESPACE){
+                            continue;
+                        }else{
+                            $annotationValue[] = $token[1];
+                        }
+                    }
+
+                }
+            }
+
+            if($errorInParsing){
+                continue;
+            }
+
+            $_annotationValidationFunction = $annotationDefinition[self::VALIDATE];
+            if(isset($_annotationValidationFunction) && is_callable($_annotationValidationFunction)){
+                $annotationValue = $_annotationValidationFunction($annotationValue);
+            }else if($annotationValue == null){
+                // check for null
+                $_allowNull = $annotationDefinition[self::ALLOW_NULL];
+                if(isset($_allowNull) && ($_allowNull == false || trim(strtoupper($_allowNull)) == "FALSE")){
+                    continue;
+                }
+            }else if(is_string($annotationValue)){
+                $_type = $annotationDefinition[self::TYPE];
+                if(isset($_type) && (trim(strtoupper($_type)) != "ANY" && trim(strtoupper($_type)) != "STRING")){
+                    continue;
+                }
+            }else{
+                $_type = $annotationDefinition[self::TYPE];
+                if(isset($_type) && (trim(strtoupper($_type)) != "ANY" && trim(strtoupper($_type)) != "ARRAY")){
+                    continue;
+                }
+            }
+
+            $annotations[$annotationName] = $annotationValue;
+        }
+
+        return $annotations;
     }
 
 
